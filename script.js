0f09beb6de63224b708543239ec012195b4b0ca7
dantecodex <anshulrajput237@gmail.com>
2024-04-05 21:38:50 +0530

    keybinding done and bug fixes

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 const myInput = document.getElementById("myInput");
 let isError = false;
 
-
+let lastPressedKey = null;
 function assign(value) {
     if (isError) {
         clearInput();
         isError = false;
     }
     myInput.value += value;
+    lastPressedKey = (myInput.value).slice(-1);
 }
 
 function clearInput() {
@@ -28,3 +29,34 @@ function calculate() {
         isError = true;
     }
 }
+
+
+const allowerdOperators = ['/', '*', '-', '+', '.'];
+function checkoperators(value) {
+    return lastPressedKey !==
+            value && allowerdOperators.indexOf(value) > -1 ||
+            (value >= 0 && value <= 9);
+}
+
+document.body.addEventListener("keydown", (value) => {
+    if (document.activeElement)
+        document.activeElement.blur();
+
+
+    if (value.key === '0') {
+        if (lastPressedKey === 'Enter')
+            clearInput();
+    }
+
+    else if (value.key === 'Enter') {
+        calculate();
+        lastPressedKey = "Enter";
+    }
+
+    else if (value.key === 'Backspace')
+        del();
+
+    else if (checkoperators(value.key))
+        assign(value.key);
+
+});
